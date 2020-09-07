47203ad0eca8514d8628a99eb0367871fba00fac
ruffatto <arthur.ruffatto@gmail.com>
2020-09-07 10:24:51 -0300

    Trabalho Template

---
+++ ProjetoSistemasWeb/ProjetoSistemasWeb.WebSite/Controllers/RelatoriosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoSistemasWeb.WebSite.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        // GET: Relatorios
+        public ActionResult Mapa()
+        {
+            return View();
+        }
+
+        public ActionResult Valores()
+        {
+            return View();
+        }
+
+        public ActionResult VariacaoDePrecos()
+        {
+            return View();
+        }
+    }
+}
